f0c1a90e85027dcff7a6fc535f56373b7a703ae9
AhmedYehiaMandwoh <ahmedyhaya91@gmail.com>
2025-01-19 13:37:51 +0200

    update session design

--- resources/views/backEnd/academics/sessions/collapse.blade.php
+++ resources/views/backEnd/academics/sessions/collapse.blade.php
@@ -1,82 +1,73 @@
 @if (count(@$menus) > 0)
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div id="accordion" class="dd">
-                        <ol class="dd-list">
-                            @foreach ($menus as $key => $element)
-                                <li class="dd-item" data-id="{{ $element->id }}">
-                                    <div class="card accordion_card" id="accordion_{{ $element->id }}">
-                                        <div class="card-header item_header" id="heading_{{ $element->id }}">
-                                            <div class="dd-handle">
-                                                <div class="pull-left">
-                                                    @lang('common.title') : {{ $element->session_name_en }}
-                                                </div>
+    <div id="accordion" class="dd">
+        <ol class="dd-list">
+            @foreach ($menus as $key => $element)
+                <li class="dd-item" data-id="{{ $element->id }}">
+                    <div class="card accordion_card" id="accordion_{{ $element->id }}">
+                        <div class=" item_header" id="heading_{{ $element->id }}">
+                            <div class="dd-handle">
+                                <div class="pull-left">
+                                    {{ $element->session_name_en }}
+                                </div>
+                            </div>
+                            <div class="pull-right btn_div">
+                                @if (userPermission('element-update'))
+                                    <a href="javascript:void(0);" 
+                                        onclick="updateSession({{ $element->id }}, '{{ $element->session_name_en }}', '{{ $element->session_name_ar }}')"
+                                        class="primary-btn btn_zindex panel-title">
+                                        @lang('common.edit')
+                                        <i class="ti-pencil-alt"></i>
+                                    </a>
+                                @endif
+                                @if (userPermission('element-update'))
+                                    <a href="javascript:void(0);" onclick="" data-toggle="collapse"
+                                        data-target="#collapse_{{ $element->id }}" aria-expanded="false"
+                                        aria-controls="collapse_{{ $element->id }}"
+                                        class="primary-btn btn_zindex panel-title">
+                                        @lang('common.show')
+                                        <i class="ti-angle-down"></i>
+                                    </a>
+                                @endif
+                                @if (env('APP_SYNC') == true)
+                                    <a href="javascript:void(0);" class="primary-btn btn_zindex mb-3"
+                                        title="Disable For Demo" data-toggle="tooltip">
+                                        <i class="ti-close"></i>
+                                    </a>
+                                @else
+                                    @if (userPermission('delete-element'))
+                                        <a href="javascript:void(0);" onclick="elementDelete({{ $element->id }})"
+                                            class="primary-btn btn_zindex mb-3">
+                                            <i class="ti-close"></i>
+                                        </a>
+                                    @endif
+                                @endif
+                            </div>
+                        </div>
+                        <div id="collapse_{{ $element->id }}" class="collapse"
+                            aria-labelledby="heading_{{ $element->id }}" data-parent="#accordion_{{ $element->id }}">
+                            <div class="card-body">
+                                <form enctype="multipart/form-data" id="elementEditForm">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                
+                                            <div>
+                                                @lang('common.session_name_en') : {{ $element->session_name_en }}
                                             </div>
-                                            <div class="pull-right btn_div">
-                                                @if (userPermission('element-update'))
-                                                    <a href="javascript:void(0);" onclick="" data-toggle="collapse"
-                                                        data-target="#collapse_{{ $element->id }}"
-                                                        aria-expanded="false"
-                                                        aria-controls="collapse_{{ $element->id }}"
-                                                        class="primary-btn btn_zindex panel-title">
-                                                        @lang('common.edit')
-                                                        <span class="collapge_arrow_normal"></span>
-                                                    </a>
-                                                @endif
-                                                @if (env('APP_SYNC') == true)
-                                                    <a href="javascript:void(0);" class="primary-btn btn_zindex"
-                                                        title="Disable For Demo" data-toggle="tooltip">
-                                                        <i class="ti-close"></i>
-                                                    </a>
-                                                @else
-                                                    @if (userPermission('delete-element'))
-                                                        <a href="javascript:void(0);"
-                                                            onclick="elementDelete({{ $element->id }})"
-                                                            class="primary-btn btn_zindex">
-                                                            <i class="ti-close"></i>
-                                                        </a>
-                                                    @endif
-                                                @endif
+                                            <div>
+                                                @lang('common.session_number') : {{ $element->session_number }}
                                             </div>
-                                        </div>
-                                        <div id="collapse_{{ $element->id }}" class="collapse"
-                                            aria-labelledby="heading_{{ $element->id }}"
-                                            data-parent="#accordion_{{ $element->id }}">
-                                            <div class="card-body">
-                                                <form enctype="multipart/form-data" id="elementEditForm">
-                                                    <div class="row">
-                                                        <div class="col-lg-6">
-                                                            <div>
-
-                                                                <button class="btn btn-primary btn-sm"
-                                                                    onclick="updateSession({{ $element->id }}, '{{ $element->session_name_en }}', '{{ $element->session_name_ar }}')">
-                                                                    Edit
-                                                                </button>
-                                                            </div>
-                                                            <div>
-                                                                @lang('common.session_name_en') : {{ $element->session_name_en }}
-                                                            </div>
-                                                            <div>
-                                                                @lang('common.session_number') : {{ $element->session_number }}
-                                                            </div>
-                                                            <div>
-                                                                @lang('common.session_ref') : {{ $element->session_ref }}
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </form>
+                                            <div>
+                                                @lang('common.session_ref') : {{ $element->session_ref }}
                                             </div>
                                         </div>
                                     </div>
-                                </li>
-                            @endforeach
-                        </ol>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </li>
+            @endforeach
+        </ol>
     </div>
 @else
     <div class="card">
@@ -86,6 +77,27 @@
     </div>
 @endif
 
+
+<style>
+    .item_header {
+        background: #415094;
+
+    }
+
+    .item_header .pull-left {
+        color: #fff;
+    }
+
+    .item_header .primary-btn {
+        color: #fff;
+
+    }
+
+    .item_header .collapge_arrow_normal {
+        color: #fff;
+
+    }
+</style>
 @push('script')
     <script>
         function updateSession(id, session_name_en, session_name_ar) {
