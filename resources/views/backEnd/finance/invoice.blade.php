53312d00a438f0cf3568f2ae19f652b4ec5baffb
AhmedYehiaMandwoh <ahmedyhaya91@gmail.com>
2025-01-22 11:54:57 +0200

    update and finish invoice

--- resources/views/backEnd/finance/invoice.blade.php
+++ resources/views/backEnd/finance/invoice.blade.php
@@ -2,6 +2,7 @@
 @section('title')
     @lang('common.finance')
 @endsection
+
 @section('mainContent')
     <section class="sms-breadcrumb mb-20">
         <div class="container-fluid">
@@ -17,6 +18,10 @@
     </section>
     <section class="admin-visitor-area up_admin_visitor up_st_admin_visitor pl_22">
         <div class="container-fluid Billig-container shadow-sm">
+            <div class="water-mark">
+                <img src="{{ asset('/uploads/settings/logo.png') }}"  alt="logo">
+
+            </div>
             <!-- Header -->
             <header>
                 <div class="row align-items-center">
@@ -25,8 +30,8 @@
 
                     </div>
                     <div class="col-5 text-end">
-                        <h4 class="mb-0 text-uppercase">Invoice</h4>
-                        <p class="mb-0">Invoice Number - 001313</p>
+                        <h4 class="mb-0 text-uppercase">Invoice NO.</h4>
+                        <p class="mb-0">NO001313</p>
                     </div>
                 </div>
                 <hr>
@@ -34,13 +39,13 @@
             <!-- Main Content -->
             <main>
                 <div class="row">
-                    <div class="col-6"><strong>Date:</strong> 05/12/2021</div>
-                    <div class="col-6 text-end"> <strong>Invoice No:</strong> 12020</div>
+                    <div class="col-6"><strong class="main-title">Date:</strong> 05/12/2021</div>
+                    <div class="col-6 text-end"> <strong class="main-title">Invoice No:</strong> 12020</div>
                 </div>
                 <hr>
                 <div class="row">
                     <div class="col-6 text-end order-sm-1">
-                        <strong>Pay To:</strong>
+                        <strong class="main-title">Pay To:</strong>
                         <address>
                             Easy Teach It Solution<br>
                             1348 Columbia Road, Denver<br>
@@ -48,7 +53,7 @@
                         </address>
                     </div>
                     <div class="col-6 order-sm-0">
-                        <strong>Invoiced To:</strong>
+                        <strong class="main-title">Invoiced To:</strong>
                         <address>
                             Jon Doe<br>
                             Street: 370 Central Avenue<br>
@@ -56,55 +61,53 @@
                         </address>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-body ">
-                        <div class="table-responsive">
-                            <table class="table mb-0">
-                                <thead>
-                                    <tr>
-                                        <td class="col-3 border-0"><strong>Service</strong></td>
-                                        <td class="col-4 border-0"><strong>Description</strong></td>
-                                        <td class="col-2 border-0"><strong>Rate</strong></td>
-                                        <td class="col-1 border-0 text-end"><strong>QTY</strong></td>
-                                        <td class="col-2 text-end border-0"><strong>Amount</strong></td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td class="col-3">SEO</td>
-                                        <td class="col-4 text-1">Optimize for search engines</td>
-                                        <td class="col-2">$520.00</td>
-                                        <td class="col-1 text-end">1</td>
-                                        <td class="col-2 text-end">$450.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="col-3">Development</td>
-                                        <td class="col-4 text-1">Website Development</td>
-                                        <td class="col-2">$100.00</td>
-                                        <td class="col-1 text-end">10</td>
-                                        <td class="col-2 text-end">$1250.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="4" class="bg-light-2 text-end"><strong>Sub Total:</strong></td>
-                                        <td class="bg-light-2 text-end">$1500.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="4" class="bg-light-2 text-end"><strong>Tax:</strong></td>
-                                        <td class="bg-light-2 text-end">$325.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="4" class="bg-light-2 text-end border-0"><strong>Total:</strong></td>
-                                        <td class="bg-light-2 text-end border-0">$1825.00</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <td class="col-3"><strong class="main-title">Service</strong></td>
+                                <td class="col-4"><strong class="main-title">Description</strong></td>
+                                <td class="col-2"><strong class="main-title">Rate</strong></td>
+                                <td class="col-1 text-end"><strong class="main-title">QTY</strong></td>
+                                <td class="col-2 text-end"><strong class="main-title">Amount</strong></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="col-3">SEO</td>
+                                <td class="col-4 text-1">Optimize for search engines</td>
+                                <td class="col-2">$520.00</td>
+                                <td class="col-1 text-end">1</td>
+                                <td class="col-2 text-end">$450.00</td>
+                            </tr>
+                            <tr>
+                                <td class="col-3">Development</td>
+                                <td class="col-4 text-1">Website Development</td>
+                                <td class="col-2">$100.00</td>
+                                <td class="col-1 text-end">10</td>
+                                <td class="col-2 text-end">$1250.00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="4" class="bg-light-2 text-end border-0"><strong class="main-title">Sub Total:</strong></td>
+                                <td class="bg-light-2 text-end border-0">$1500.00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="4" class="bg-light-2 text-end border-0"><strong class="main-title">Tax:</strong></td>
+                                <td class="bg-light-2 text-end border-0">$325.00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="4" class="bg-light-2 text-end border-0"><strong class="main-title">Total:</strong></td>
+                                <td class="bg-light-2 text-end border-0">$1825.00</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </main>
+
+
             <!-- Footer -->
             <footer class="text-center mt-4">
-                <p class="text-1"><strong>NOTE :</strong> This is computer generated receipt and does not require physical
+                <p class="text-1"><strong class="main-title">NOTE :</strong> This is computer generated receipt and does not require physical
                     signature.</p>
                 <div class="btn-group btn-group-sm d-print-none"> 
                     <a href="#"
@@ -116,3 +119,41 @@
         </div>
     </section>
 @endsection
+
+
+<style>
+    th {
+        font-size: 14px !important;
+
+    }
+    td {
+        padding: 10px !important;
+        font-size: 12px !important;
+        color: #333 !important;
+
+    }
+    .Billig-container {
+
+        background: #ffff;
+        padding: 30px;
+    }
+
+    .main-title {
+        font-size: 14px !important;
+        color: #3c4e7a;
+    }
+    address,p,h4 {
+
+    color: #333 !important;
+    }
+
+    .water-mark {
+        position: absolute;
+        bottom: 50px;
+    }
+    .water-mark img{
+        width: 100%;
+    opacity: 0.1;
+    transform: rotate(15deg);
+    }
+</style>
