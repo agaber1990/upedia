9c6d5c94cb3d323d65101d5685a06622ac8268d4
AhmedYehiaMandwoh <ahmedyhaya91@gmail.com>
2025-02-02 17:51:38 +0200

    update and finish

--- resources/views/backEnd/finance/create.blade.php
+++ resources/views/backEnd/finance/create.blade.php
@@ -17,128 +17,155 @@
     </section>
     <section class="admin-visitor-area up_admin_visitor up_st_admin_visitor pl_22">
         <div class="container-fluid p-0">
-           
-            <div class="row">
-                <div class="primary_input col-md-4">
-                    <label class="primary_input_label" for="student_id">
-                        @lang('common.select_student') <span class="text-danger">*</span>
-                    </label>
-                    <select class="primary_select form-control" name="student_id" id="student_id">
-                        <option value="">@lang('common.select_student')</option>
-                        @foreach ($students as $student)
-                            <option value="{{ $student->id }}">{{ $student->full_name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="primary_input col-md-4">
-                    <label class="primary_input_label" for="track">
-                        @lang('common.pricing_plan_type') <span class="text-danger">*</span>
-                    </label>
-                    <select class="primary_select form-control" name="pricing_plan_type" id="pricing_plan_type">
-                        <option value="">@lang('common.select')</option>
-                        @foreach ($pricing_types as $type)
-                            <option value="{{ $type->id }}" >
-                                {{ $type->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="primary_input col-md-4">
-                    <label class="primary_input_label" for="">@lang('common.due_date')
-                        <span class="text-danger"> *</span></label>
 
-                        
-                    <input class="primary_input_field  inputs" type="date" value="{{ date('Y-m-d')}}" min="{{ date('Y-m-d')}}"
-                        name="due_date" id="due_date" />
-                </div>
-                <div class="primary_input col-md-4 mt-2">
-                    <div class="d-flex">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="track" name="">
-                            <label class="form-check-label text-dark fw-bold" for="track">
-                                <i class="bi bi-check-circle-fill text-success me-1"></i> 
-                            </label>
-                        </div>
-                        <label class="primary_input_label" for="">@lang('common.posting_date')
-                            <span class="text-danger"> *</span></label>
+            <div id="accordion">
+                <div class="card">
+                    <div class="card-header pt-0 pb-0" id="headingOne">
+                        <h5 class="mb-0 create-title" data-toggle="collapse" data-target="#collapseOne"
+                            aria-expanded="false" aria-controls="collapseOne">
+                            <button class="btn btn-link add_btn_link">
+                               info
+                            </button>
+                        </h5>
                     </div>
-                    <input class="primary_input_field  inputs" type="date" disabled
-                        name="posting_date" id="posting_date" />
-                </div>
-          
-                <div class="primary_input col-md-12 mt-2" >
-                    <hr>
-                    {{-- <div style="text-align: end">
-                        <button type="button" class="primary-btn fix-gr-bg  mb-2 p-3">
-                        <i class="fas fa-plus mx-0"></i>   
-                         </button>
-                    </div> --}}
-                    <table class="table table-bordered bg-white">
-                        <thead>
-                            <tr>
-                                <th style="    width: 110px;">
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="track" name="">
-                                        <label class="form-check-label text-dark fw-bold" for="track">
-                                            <i class="bi bi-check-circle-fill text-success me-1"></i> 
-                                        </label>
-                                        {{__('common.check_all')}}
-                                    </div>
-                                </th>
-                                <th>{{__('common.select_tack_name')}}</th>
-                                <th>{{__('common.levels_qty')}}</th>
-                                <th>{{__('common.price')}}</th>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="track" name="">
-                                        <label class="form-check-label text-dark fw-bold" for="track">
-                                            <i class="bi bi-check-circle-fill text-success me-1"></i> 
-                                        </label>
-                                    </div>
-                                </td>
-                                <td >
-                                    <label class="primary_input_label" for="track">
-                                        @lang('common.select_track') <span class="text-danger">*</span>
+                    <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="primary_input col-md-4">
+                                    <label class="primary_input_label" for="student_id">
+                                        @lang('common.select_student') <span class="text-danger">*</span>
                                     </label>
-                                    <select class="primary_select form-control" name="track_id" id="track">
-                                        <option value="">@lang('common.select_track')</option>
-                                        @foreach ($tracks as $track)
-                                            <option value="{{ $track->id }}" data-level="{{ $track->level_number }}">
-                                                {{ $track->track_name_en }}
+                                    <select class="primary_select form-control" name="student_id" id="student_id">
+                                        <option value="">@lang('common.select_student')</option>
+                                        @foreach ($students as $student)
+                                            <option value="{{ $student->id }}">{{ $student->full_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="primary_input col-md-4">
+                                    <label class="primary_input_label" for="track">
+                                        @lang('common.pricing_plan_type') <span class="text-danger">*</span>
+                                    </label>
+                                    <select class="primary_select form-control" name="pricing_plan_type" id="pricing_plan_type">
+                                        <option value="">@lang('common.select')</option>
+                                        @foreach ($pricing_types as $type)
+                                            <option value="{{ $type->id }}">
+                                                {{ $type->name }}
                                             </option>
                                         @endforeach
                                     </select>
-                                </td>
-                                <td>
-                                    <div class="primary_input mb-2">
-                                        <label class="primary_input_label" for="">@lang('common.levels')
+                                </div>
+                                <div class="primary_input col-md-4">
+                                    <label class="primary_input_label" for="">@lang('common.due_date')
+                                        <span class="text-danger"> *</span></label>
+                
+                
+                                    <input class="primary_input_field  inputs" type="date" value="{{ date('Y-m-d') }}"
+                                        min="{{ date('Y-m-d') }}" name="due_date" id="due_date" />
+                                </div>
+                                <div class="primary_input col-md-4 mt-2">
+                                    <div class="d-flex">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="track" name="">
+                                            <label class="form-check-label text-dark fw-bold" for="track">
+                                                <i class="bi bi-check-circle-fill text-success me-1"></i>
+                                            </label>
+                                        </div>
+                                        <label class="primary_input_label" for="">@lang('common.posting_date')
                                             <span class="text-danger"> *</span></label>
-                                        <input class="primary_input_field form-control "
-                                            name="levels" id="levels" />
                                     </div>
-                                </td>
-                                <td>
-                                    350 EGP
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                    <input class="primary_input_field  inputs" type="date" disabled name="posting_date"
+                                        id="posting_date" />
+                                </div>
+                
+                             
+                            </div>
+                        </div>
+                    </div>
+                
                 </div>
-                <input type="hidden" id="levels_id" name="levels_id" value="">
+                <div class="card mt-2">
+                    <div class="card-header pt-0 pb-0" id="headingTwo">
+                        <h5 class="mb-0 create-title" data-toggle="collapse" data-target="#collapseTwo"
+                            aria-expanded="false" aria-controls="collapseTwo">
+                            <button class="btn btn-link add_btn_link">
+                               Tracks and levels
+                            </button>
+                        </h5>
+                    </div>
+
+              
+                    <div id="collapseTwo" class="collapse " aria-labelledby="headingTwo" data-parent="#accordion">
+                        <div class="table-responsive">
+                             
+                            <table class="table table-bordered bg-white">
+                                <thead>
+                                    <tr>
+                                        <th style="    width: 110px;">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="track" name="">
+                                                <label class="form-check-label text-dark fw-bold" for="track">
+                                                    <i class="bi bi-check-circle-fill text-success me-1"></i>
+                                                </label>
+                                                {{ __('common.check_all') }}
+                                            </div>
+                                        </th>
+                                        <th>{{ __('common.select_tack_name') }}</th>
+                                        <th>{{ __('common.levels_qty') }}</th>
+                                        <th>{{ __('common.price') }}</th>
+        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="track" name="">
+                                                <label class="form-check-label text-dark fw-bold" for="track">
+                                                    <i class="bi bi-check-circle-fill text-success me-1"></i>
+                                                </label>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <label class="primary_input_label" for="track">
+                                                @lang('common.select_track') <span class="text-danger">*</span>
+                                            </label>
+                                            <select class="primary_select form-control" name="track_id" id="track">
+                                                <option value="">@lang('common.select_track')</option>
+                                                @foreach ($tracks as $track)
+                                                    <option value="{{ $track->id }}" data-level="{{ $track->level_number }}">
+                                                        {{ $track->track_name_en }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <div class="primary_input mb-2">
+                                                <label class="primary_input_label" for="">@lang('common.levels')
+                                                    <span class="text-danger"> *</span></label>
+                                                <input class="primary_input_field form-control " name="levels" id="levels" />
+                                            </div>
+                                        </td>
+                                        <td>
+                                            350 EGP
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+      
         </div>
     </section>
 @endsection
 
 <style>
-      .table thead th,
+    .table thead th,
     .table tbody td {
-        padding: 10px !important 
+        padding: 10px !important
     }
 
     .inputs {
@@ -147,10 +174,23 @@
         color: #415094 !important;
         border: 1px solid #d4d4d4 !important;
     }
+    .card-header{
+        background: #f9f9f9;
+    }
+    .card-header h5 {
+        font-size: 14px !important
+    }
+    .card-header button {
+        color: #415094  !important;
+        text-decoration: none  !important;
+    }
+    .add_btn_link {
+        font-size: 14px !important
+
+    }
 </style>
 @push('scripts')
     <script>
-
         $('#createInvoiceForm').on('click', function() {
             let staff_scheduleds_id = $('#staff_scheduleds_id').val();
             let student_id = $('#student_id').val();
