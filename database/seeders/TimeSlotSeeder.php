5339db8e50487a03d3585922b86913ba87c7d7b9
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-06-01 15:40:52 +0300

    update slots

--- database/seeders/TimeSlotSeeder.php
+++ database/seeders/TimeSlotSeeder.php
@@ -14,24 +14,32 @@ class TimeSlotSeeder extends Seeder
      */
     public function run()
     {
-        $days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $days = [ 'Saturday','Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday'];
         $timeSlots = [];
 
         foreach ($days as $day) {
-            for ($hour = 0; $hour < 23; $hour++) {
+            for ($hour = 10; $hour < 24; $hour++) {
                 // Generate start and end times for each hour
                 $startTime = sprintf('%02d:00:00', $hour); // E.g., "00:00:00"
                 $endTime = sprintf('%02d:00:00', ($hour + 1) % 24); // E.g., "01:00:00", wrapping to "00:00:00" at midnight
 
-                $timeSlots[] = [
+                $teacherTimeSlots[] = [
                     'slot_day' => $day,
                     'slot_start' => $startTime,
                     'slot_end' => $endTime,
+                    'type'=>'teacher'
+                ];
+                $testerTimeSlots[] = [
+                    'slot_day' => $day,
+                    'slot_start' => $startTime,
+                    'slot_end' => $endTime,
+                    'type'=>'tester'
                 ];
             }
         }
 
         // Insert the time slots into the database
-        DB::table('slot_emps')->insert($timeSlots);
+        DB::table('slot_emps')->insert(array_merge($teacherTimeSlots, $testerTimeSlots));
+
     }
 }
